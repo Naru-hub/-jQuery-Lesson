5d4690eeccbf574c0d1888d25086c2a16df0e8d7
Naru-hub <1208ny0324@gmail.com>
2021-05-31 16:17:16 +0900

    mouseover mouseout css

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $(function(){
- $('.box1').slideDown(function(){
+ $('.box1').mouseover(function(){
   $('.box1').css({
-    'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
- }).slideUp();
+    'background-color': '#0000FF'});
+});
+$('.box1').mouseout(function(){
+  $('.box1').css({'background-color':'#FF0000'});
 });
 });
 
